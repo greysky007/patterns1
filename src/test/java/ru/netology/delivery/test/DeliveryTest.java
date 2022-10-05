96efc20204077ccfe785ade3ed2c8155412cd7b4
Антон Юранов <antonysnork@gmail.com>
2022-10-05 11:52:20 +0600

    reformated

--- src/test/java/ru/netology/delivery/test/DeliveryTest.java
+++ src/test/java/ru/netology/delivery/test/DeliveryTest.java
@@ -36,10 +36,6 @@ class DeliveryTest {
         $("span [placeholder=\"Дата встречи\"]").sendKeys(Keys.CONTROL, "a", Keys.DELETE);
         $("span [placeholder=\"Дата встречи\"]").setValue(secondMeetingDate);
         $x("//span[@class='button__text'][text()='Перепланировать']").click();
-        // TODO: добавить логику теста в рамках которого будет выполнено планирование и перепланирование встречи.
-        // Для заполнения полей формы можно использовать пользователя validUser и строки с датами в переменных
-        // firstMeetingDate и secondMeetingDate. Можно также вызывать методы generateCity(locale),
-        // generateName(locale), generatePhone(locale) для генерации и получения в тесте соответственно города,
-        // имени и номера телефона без создания пользователя в методе generateUser(String locale) в датагенераторе
+
     }
 }
