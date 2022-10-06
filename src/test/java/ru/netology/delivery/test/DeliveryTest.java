cfa0f641f99842df099ea688af660ef1abf86dc2
Антон Юранов <antonysnork@gmail.com>
2022-10-06 15:53:02 +0600

    test fixed

--- src/test/java/ru/netology/delivery/test/DeliveryTest.java
+++ src/test/java/ru/netology/delivery/test/DeliveryTest.java
@@ -1,12 +1,16 @@
 package ru.netology.delivery.test;
 
 import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 import ru.netology.delivery.data.DataGenerator;
 
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 
 class DeliveryTest {
@@ -32,10 +36,15 @@ class DeliveryTest {
         $("span [name=\"name\"]").setValue(validUser.getName());
         $(".checkbox__box").click();
         $(".button__text").click();
+        String text = $x("//div[(text()='Встреча успешно запланирована на ')]").should(visible).getText();
+        System.out.println(text);
+        Assertions.assertEquals("Встреча успешно запланирована на "+ firstMeetingDate, text);
         $(".button__text").click();
         $("span [placeholder=\"Дата встречи\"]").sendKeys(Keys.CONTROL, "a", Keys.DELETE);
         $("span [placeholder=\"Дата встречи\"]").setValue(secondMeetingDate);
         $x("//span[@class='button__text'][text()='Перепланировать']").click();
+        String textNew = $x("//div[(text()='Встреча успешно запланирована на ')]").should(visible).getText();
+        Assertions.assertEquals("Встреча успешно запланирована на "+ secondMeetingDate, textNew);
 
     }
 }
